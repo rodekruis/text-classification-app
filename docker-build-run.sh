2f3dbc37eda36fe98fbec11b245506263d952ce9
jmargutti <jacopo.margutti@gmail.com>
2022-09-04 15:23:18 +0200

    add startup script

---
+++ docker-build-run.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+cd $HOME/text-classification-app || return
+sudo git pull
+sudo docker system prune -a -f
+sudo docker build -t text-classification-app .
+sudo docker run --name text-classification-app --gpus all -p 80:8000 -t text-classification-app
