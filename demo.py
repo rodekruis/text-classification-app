c956c39db97aeb3ff49b8114bdc33dd0686f6fbb
jmargutti <jacopo.margutti@gmail.com>
2022-09-04 12:33:18 +0200

    add demo

--- demo.py
+++ demo.py
@@ -1,34 +1,30 @@
 import streamlit as st
 import requests
-
-
-def build_prompt(topic, input_text):
-    model_prompt = f"Message: {input_text} "
-    model_prompt += f"Is this message about {topic}?"
-    return model_prompt
+import seaborn as sns
+import matplotlib.pyplot as plt
+import numpy as np
+sns.set_theme(style="whitegrid")
 
 model_req = requests.get('http://language-model.westeurope.cloudapp.azure.com/model').json()
 
 st.header("text-classification-app")
 st.markdown(f"Use language models to classify text. Currently powered by [{model_req['model_name']}]({model_req['source']}).")
 st.markdown("Built with love by [NLRC 510](https://www.510.global/). See [the project on GitHub](https://github.com/rodekruis/text-generation-app).")
-input_topic = st.text_area('Input topic', 'World')
-input_text = st.text_area('Input text', 'Hello world!')
+input_text = st.text_area('Text', 'I have a problem with my iphone that needs to be resolved asap!!')
+input_labels = st.text_area('Class labels (comma-separated)', 'urgent, not urgent, phone, tablet, computer')
 submit = st.button('Classify')
 if submit:
 
-    url = 'http://language-model.westeurope.cloudapp.azure.com/generate/'
+    url = 'http://language-model.westeurope.cloudapp.azure.com/classify/'
     payload = {
-        'text': build_prompt(input_topic.lower(), input_text),
-        'length': 30
+        'text': input_text,
+        'labels': input_labels.split(','),
+        'multi_label': True
     }
-
     with st.spinner(text="This may take a moment..."):
-        result = requests.post(url, json=payload).json()
-        print(payload, result)
-        if 'generated_text' in result.keys():
-            text = result['generated_text']
-        else:
-            text = "An error occurred, please try again later or [contact us](mailto:support@510.global)!"
-
-    st.text_area('Output', text)
+        results = requests.post(url, json=payload).json()
+        fig, ax = plt.subplots()
+        scores = [x*100. for x in results['scores']]
+        sns.barplot(x=scores, y=results['labels'], label="Total", color="b")
+        ax.set(xlabel='score (%)', ylabel='class labels')
+        st.pyplot(fig)
